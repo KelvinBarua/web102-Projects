5767a1358eece8ed9abcb3eeb4cee7339a079c61
Kelvin Barua <kelvinbarua78@gmail.com>
2024-03-29 23:40:38 -0400

    03-29-2024__11:40PM

--- Project5/src/App.jsx
+++ Project5/src/App.jsx
@@ -4,15 +4,43 @@ import './App.css'
 import ComicData from './components/ComicData'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  const [searchInput, setSearchInput] = useState('');
+
+
+  const handleInputChange = (e) => {
+    setSearchInput(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setSearchInput('');
+  }
 
   return (
     <>
       <div className="container">
-        <div className="navbar-container"></div>
+        <div className="navbar-container">
+          <h1>Navbar</h1>
+        </div>
 
         <div className="data-container">
-          <ComicData />
+          <div className ="stats-container">
+            <h3>Total Comics 📚: {count}</h3>
+            <h3>Comics Date-range 🗓️: </h3>
+            <form>
+              <input
+              className='search-input' 
+              type='text' 
+              placeholder='search'
+              id='search-input'
+              value={searchInput}
+              onChange={handleInputChange}></input>
+              <button onClick={handleSubmit} type='submit'>Search</button>
+            </form>
+          </div>
+          <ComicData setCount={setCount} userInput={searchInput}/>
         </div>
       </div>
     </>
--- Project5/src/components/ComicData.jsx
+++ Project5/src/components/ComicData.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import md5 from 'md5';
+import titleTracker from './titleTracker';
 
 const API_KEY = '74e59ba98ef13b65b7f9463b1922cc5b';
 const PRIVATE_KEY = '575f3182cbf7be8ede606d94df460e7f69246d72';
@@ -11,10 +12,12 @@ const limit = 100;
 const url = `https://gateway.marvel.com:443/v1/public/comics?dateRange=1945-01-01%2C2024-03-28&limit=${limit}&apikey=${API_KEY}&ts=${timestamp}&hash=${hash}&format=comic&formatType=comic`;
 
 
-const ComicData = () => {
+const ComicData = ({setCount, userInput}) => {
   const [comics, setComics] = useState([]);
+  const [comicTitles, setComicTitles] = useState([]);
   const [error, setError] = useState('');
   const [offset, setOffset] = useState(0);
+  const [filteredComics, setFilteredComics] = useState([]);
 
   useEffect(() => {
     const fetchComic = async () =>{
@@ -28,30 +31,49 @@ const ComicData = () => {
             offset: offset,
           }
         });
-        const comics_data = response.data.data.results.filter(comic => comicHasCoverImage(comic));
+        const comics_data = response.data.data.results.filter(comic => (!titleTracker.includes(comic.title)));
+
+        const titles = comics_data.map(comic => comic.title);
+
+        titles.forEach(title => { //study this branch later, this pushes all comic titles to the array i created...
+          if(!titleTracker.includes(title)){
+            titleTracker.push(title);
+            setComicTitles([...titleTracker]);
+          }
+        });
+
         setComics(prevComics => [...prevComics, ...comics_data]);
         setOffset(prevOffset => prevOffset + limit);
+        console.log(comicTitles);
       } catch (error) {
         setError("Couldn't fetch data :(");
         console.error(error);
       }
     }
     fetchComic();
-    console.log(url);
+    for(var i = 0; i < titleTracker.length; i++){
+      titleTracker.pop();
+    }
   }, []);
 
-  const comicHasCoverImage = (comic) =>{ //function will check if the a comic has a cover image or not. 
-    return comic.thumbnail && comic.thumbnail.path && comic.thumbnail.extension;
-  }
+  useEffect(() => {
+    if(userInput) {
+      const filtered = comics.filter(comic => comic.title.toLowerCase().includes(userInput.toLowerCase()));
+      setFilteredComics(filtered);
+    } else {
+      setFilteredComics(comics);
+    }
+  }, [userInput, comics]);
 
   return (
     <div className='comic-component'>
       {error && <p>{error}</p>}
       {comics.length > 0 ? (
         <div className='comics-grid'>
-          {comics.map((comic) => (
-            <div key={comic.id} className='comics-container'>
-              <p className='comic-title'>{comic.title}</p>
+          {setCount(titleTracker.length)}
+          {filteredComics.map((comic, index) => (
+            <div key={index} className='comics-container'>
+              <h3 className='comic-title'>{comic.title}</h3>
               <img id='comic-img' src={`${comic.thumbnail.path}.${comic.thumbnail.extension}`} />
             </div>
           ))}
