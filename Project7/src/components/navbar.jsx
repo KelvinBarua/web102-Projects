9f23dd47745e3192a56fa295d8c9b957af50f56e
Kelvin Barua <kelvinbarua78@gmail.com>
2024-04-11 18:17:15 -0400

    04-11-2024__6:17PM

--- Project7/src/components/navbar.jsx
+++ Project7/src/components/navbar.jsx
@@ -1,10 +1,12 @@
 import { Link } from "react-router-dom";
 import Home_btn from "../assets/home_btn.png";
+import Create_btn from "../assets/Create-a-crewmate-btn.png";
 
 const Navbar = () =>{
   return (
     <div className="navbar">
-      <Link id="HomeLink" to="/"><img id="HomeBtn" src={Home_btn} /></Link>
+      <Link id="HomeLink" to="/"><img id="NavBtn" src={Home_btn} /></Link>
+      <Link id="CreateLink"><img id="NavBtn" src={Create_btn} /></Link>
     </div>
   )
 }
--- Project7/src/pages/Home.jsx
+++ Project7/src/pages/Home.jsx
@@ -7,6 +7,17 @@ function Home() {
   return (
     <div className="container">
       <Navbar />
+      <div className='main-area'>
+        <h1 className='main-title'>Welcome to the Crewmate Creator!</h1>
+        <h3>Create a custom crewmate lobby before deploying them into the Among Us Games!</h3>
+
+        <div className="crewmateModArea">
+          <form id="create-form">
+            <label for="name-field">Crewmate Name: </label>
+            <input id="name-field" type="text"></input>
+          </form>
+        </div>
+      </div>
     </div>
   )
 }
