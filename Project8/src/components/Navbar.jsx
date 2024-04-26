2fdfd00579bc21b3b3e99d9d42e8e51e66478eb3
Kelvin Barua <kelvinbarua78@gmail.com>
2024-04-26 02:24:32 -0400

    04-26-2024__2:24PM

--- Project8/src/components/Navbar.jsx
+++ Project8/src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = ({searchInput, setSearchInput}) => {
 
       <div className="search-form-container">
         <form className="search-form">
-          <label for="search-input">Search</label>
+          <label id="search-bar-label" for="search-input">Search</label>
           <input 
           type="text" 
           id="search-input"
@@ -17,6 +17,8 @@ const Navbar = ({searchInput, setSearchInput}) => {
           onChange={(e) => {setSearchInput(e.target.value)}}></input>
         </form>
       </div>
+
+      <h1>💪 StrengthSquad Formus 💪</h1>
     </div>
   )
 }
